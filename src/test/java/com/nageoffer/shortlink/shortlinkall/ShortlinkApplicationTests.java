4d3e239b07fadc24a45bbb00fc00f8dac7602bae
NanaSakiAi1 <dnd5605@icloud.com>
2025-09-29 15:02:24 +0800

    Initial 2

---
+++ src/test/java/com/nageoffer/shortlink/shortlinkall/ShortlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nageoffer.shortlink.shortlinkall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
