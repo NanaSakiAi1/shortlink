311df01687cdae9a3969cd0b8089f7d7e4d3e4ee
NanaSakiAi1 <dnd5605@icloud.com>
2025-10-04 14:02:07 +0800

    t_group分表

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -1,20 +1,18 @@
 package com.nageoffer.shortlink.admin.test;
 
 public class UserTableShardingTest {
-    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
-            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',\n" +
+    public static final String SQL = "CREATE TABLE `t_group-%d` (\n" +
+            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `gid` varchar(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '分组标识',\n" +
+            "  `name` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '分组名称\\n',\n" +
+            "  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建分组用户名\\n',\n" +
+            "  `sort_order` int DEFAULT NULL COMMENT '分组排序',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1715030926162935810 DEFAULT CHARSET=utf8mb4;";
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
