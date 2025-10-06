10c51dfe737a82e739586dbb2b6bc15319370a44
NanaSakiAi1 <dnd5605@icloud.com>
2025-10-06 20:41:49 +0800

    短连接统计访问网络信息

---
+++ project/src/main/java/com/nageoffer/shortlink/shortlinkporject/dao/entity/LinkNetworkStatsDO.java
@@ -0,0 +1,49 @@
+package com.nageoffer.shortlink.shortlinkporject.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.shortlinkporject.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 访问网络统计访问实体
+ *
+ */
+@Data
+@TableName("t_link_network_stats")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkNetworkStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+
+    private String gid;
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 访问网络
+     */
+    private String network;
+}
